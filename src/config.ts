6c1148bb36fa7b81ff57782fc91e4d0f0d9b7cca
goenning <me@goenning.net>
2016-11-16 19:37:35 -0200

    reuse db parameters from migration file

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,12 @@
+import { join } from 'path';
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (process.env.NODE_ENV === 'development') {
-  process.env.DATABASE_URL = 'postgres://weedo:weedo-pw@localhost:5555/weedo';
-} else if (process.env.NODE_ENV === 'test') {
-  process.env.DATABASE_URL = 'postgres://weedo-test:weedo-test-pw@localhost:5556/weedo-test';
+const databases = require(join(process.cwd(), 'database.json'));
+const connectionString = databases[process.env.NODE_ENV];
+
+if (typeof connectionString === 'string') {
+  process.env.DATABASE_URL = connectionString;
 }
 
 export default {
