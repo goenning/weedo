8f2185a742de6a1b736992cf8be930024e42edce
goenning <me@goenning.net>
2016-11-19 20:48:36 -0200

    build assets

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,17 @@ export const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use('/public', express.static('public'));
 
 app.use(async (req, res, next) => {
   const host = `${req.protocol}://${req.get('host')}`;
-  res.locals.settings = await getSiteSettings(host);
-  if (!res.locals.settings) {
+  res.locals.site = await getSiteSettings(host);
+  if (!res.locals.site) {
     return res.status(404).send();
   }
   next();
 });
 
 app.get('/', (req, res, next) => {
-  res.render('index', { message: res.locals.settings });
+  res.render('index', { });
 });
